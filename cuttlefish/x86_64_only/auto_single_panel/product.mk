46ec86e17c7aaeb580df6dc3139e3a7b79898451
DonizeteVida <donizetevida1998@outlook.com>
2026-04-30 20:45:45 +0200

    Move folders

--- cuttlefish/x86_64_only/auto_single_panel/product.mk
+++ cuttlefish/x86_64_only/auto_single_panel/product.mk
@@ -2,13 +2,12 @@ PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
 
 $(call inherit-product, device/google/cuttlefish/vsoc_x86_64_only/auto/aosp_cf.mk)
 
-# BEGIN		display_config
 $(call soong_config_append, cvd, launch_configs, cvd_config_single_panel.json)
 TARGET_BOARD_INFO_FILE := device/donizete/cuttlefish/x86_64_only/auto_single_panel/android-info.txt
-# END		display_config
 
 PRODUCT_PACKAGES += \
-	donizete_remove_products
+	donizete_remove_products \
+	donizete_sample_app
 
 PRODUCT_NAME := auto_single_panel
 PRODUCT_DEVICE := x86_64_only
